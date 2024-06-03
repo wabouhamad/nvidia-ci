02b7ce4090f242f95d639934128db7e3e1da7ada
wabouhamad <wabouham@redhat.com>
2024-06-03 15:27:17 -0400

    go.mod go.sum and vendor commit

--- vendor/github.com/openshift-kni/eco-goinfra/pkg/nodes/nodetypes.go
+++
@@ -1,9 +0,0 @@
-package nodes
-
-// ExternalNetworks contains external node ip4/ipv6 addresses.
-type ExternalNetworks struct {
-	IPv4 string `json:"ipv4,omitempty"`
-	IPv6 string `json:"ipv6,omitempty"`
-}
-
-const ovnExternalAddresses = "k8s.ovn.org/node-primary-ifaddr"
